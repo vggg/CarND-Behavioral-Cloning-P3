8a9564dd65b63a4b23993319e3e1673cecf56c35
Vikram Godbole <vikram.godbole@shalkiengineers.com>
2017-10-31 06:50:31 +0000

    cropping image

--- clone.py
+++ clone.py
@@ -11,16 +11,21 @@ with open('../data/driving_log.csv') as csvfile:
 
 images = []
 measurements = []
-
+correction = 0.2
 
 for line in lines:
-  source_path = line[0]
-  filename = source_path.split('/')[-1]
-  current_path = '../data/IMG/' + filename
-  image = cv2.imread(current_path)
-  images.append(image)
-  measurement = float(line[3])
-  measurements.append(measurement)
+  for i in range(3):
+    source_path = line[i]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    if i is 0:
+    	measurement = float(line[3])
+    	measurements.append(measurement)
+    else:
+    	measurement = float(line[3]) * ((-1)**i * correction)
+    	measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
 for image,measurement in zip(images, measurements):
@@ -33,12 +38,13 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(6,5,5, activation="relu"))
 model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5, activation="relu"))
