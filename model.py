edf2fdfff38d587a2d10aa64f7984154f8e9ed1d
Vikram Godbole <VGodbole@walmartlabs.com>
2017-11-07 00:01:07 -0800

    add viz loss plot

--- model.py
+++ model.py
@@ -124,13 +124,35 @@ model.add(Dense(50))
 model.add(Dense(10))
 model.add(Dense(1))
 
+
 model.compile(loss='mse', optimizer='adam')
 #		samples_per_epoch=len(train_samples),
 #model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3)
-model.fit_generator(train_generator, 
-                samples_per_epoch = (len(train_samples)//batch_size)*batch_size,
-		validation_data=validation_generator, 
-            	nb_val_samples=len(validation_samples),
-		nb_epoch=5)
+#model.fit_generator(train_generator, 
+#                samples_per_epoch = (len(train_samples)//batch_size)*batch_size,
+#		validation_data=validation_generator, 
+#            	nb_val_samples=len(validation_samples),
+#		nb_epoch=5)
+
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+history_object = model.fit_generator(train_generator, samples_per_epoch =
+    (len(train_samples)//batchsize)*batch_size, validation_data = 
+    validation_generator,
+    nb_val_samples = len(validation_samples), 
+    nb_epoch=5, verbose=1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
 
 model.save('model.h5')
